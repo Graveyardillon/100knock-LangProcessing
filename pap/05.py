731078761fa9dedf751a4a6592ad7ccfed606113
Papillon6814 <so1rou.k@gmail.com>
2020-02-16 01:56:24 +0900

    Update 05.py

--- pap/05.py
+++ pap/05.py
@@ -1,1 +1,17 @@
-# FIXME
+SENTENCE = "I am an NLPer"
+N = 2
+
+def ngram(s):
+  c = []
+  w = []
+  sp = s.split()
+  for i in range(len(s)):
+    c.append(s[i:i+N])
+  
+  for i in range(len(sp)):
+    w.append(sp[i:i+N])
+  
+  print(c)
+  print(w)
+
+ngram(SENTENCE)
